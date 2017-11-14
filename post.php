b6be0f6d715c0a390e1b95047a8ebeeabaf27f1b
vcruz2002 <31362121+vcruz2002@users.noreply.github.com>
2017-11-13 21:10:39 -0800

    Update post.php

--- post.php
+++ post.php
@@ -3,7 +3,7 @@ session_start();
 if(isset($_SESSION['name'])){
     $text = $_POST['text'];
      
-    $fp = fopen("log.html", 'a');
+    $fp = fopen("index.html", 'a');
     fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
     fclose($fp);
 }
